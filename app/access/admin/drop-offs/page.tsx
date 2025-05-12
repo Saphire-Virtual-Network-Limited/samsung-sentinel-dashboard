bd573870638d8c2902981c78294d85e55da08a61
Thompson Opeyemi <Topeyemi33@gmail.com>
2025-05-12 21:08:19 +0100

    admin drop-offs

---
+++ app/access/admin/drop-offs/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+import { DropOffsView } from '@/view/dashboard';
+
+const Page = () => {
+  return (
+    <div>
+      <DropOffsView />
+    </div>
+  );
+};
+
+export default Page;
