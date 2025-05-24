6566db8e1157a46b293aae8cb231e8e05ec81ddf
Thompson Opeyemi <Topeyemi33@gmail.com>
2025-05-24 11:19:41 +0100

    update

---
+++ view/dashboard/devices/deviceSummary.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { Tabs, Tab, cn } from "@heroui/react";
+import { GeneralSans_Meduim } from "@/lib";
+import { DefaultedView, AllDevices, EnrolledDeviceView, UnEnrolledDeviceView } from "@/view/dashboard";
+
+
+import React from "react";
+
+const DeviceSummaryView = () => {     
+	return (
+		<div className="flex w-full flex-col">
+			<Tabs
+				aria-label="Options"
+				size="lg"
+				radius="lg"
+				color="primary"
+				className={cn("pb-5", GeneralSans_Meduim.className)}
+				classNames={{
+					tab: "lg:p-4 text-sm lg:text-base",
+				}}>
+				<Tab
+					key="All Loans"
+					title="All Loans"
+					className="lg:p-4 text-base">
+					<AllDevices />	
+				</Tab>
+				<Tab
+					key="Enrolled"
+					title="Enrolled"
+					className="lg:p-4 text-base">
+					<EnrolledDeviceView />
+				</Tab>
+                    <Tab
+                        key="Approved"
+                        title="Approved"
+                        className="lg:p-4 text-base">
+                        <UnEnrolledDeviceView />
+                    </Tab>
+                    <Tab
+                        key="Defaulted"
+                        title="Defaulted"
+                        className="lg:p-4 text-base">
+                        <DefaultedView />
+                    </Tab>
+
+			</Tabs>
+		</div>
+	);
+};
+
+export default DeviceSummaryView;
---
+++ view/dashboard/devices/unEnrolledDeviceView.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UnEnrolledDeviceView = () => {
+  return (
+    <div>unEnrolledDeviceView</div>
+  )
+}
+
+export default UnEnrolledDeviceView
