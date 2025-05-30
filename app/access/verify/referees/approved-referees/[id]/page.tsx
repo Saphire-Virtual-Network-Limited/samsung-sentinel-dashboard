3b2294200131afe484b1324746e29bb889e0702c
Greatness Abolade <49532326+thegrtnx@users.noreply.github.com>
2025-05-30 07:36:10 +0100

    Update page.tsx

--- app/access/verify/referees/approved-referees/[id]/page.tsx
+++ app/access/verify/referees/approved-referees/[id]/page.tsx
@@ -1,16 +1,12 @@
-"use client";
-
 import React from "react";
 import SingleRefereeView from "@/view/dashboard/verify-Referee/singleRefView";
 
 interface PageProps {
-  params: Promise<{
-    id: string;
-  }>;
-  searchParams?: { [key: string]: string | string[] | undefined };
+  params: Promise<{ id: string }>;
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
-export default function ApprovedRefereePage({ params }: PageProps) {
-  const resolvedParams = React.use(params);
+export default async function ApprovedRefereePage({ params }: PageProps) {
+  const resolvedParams = await params;
   return <SingleRefereeView id={resolvedParams.id} />;
 }
