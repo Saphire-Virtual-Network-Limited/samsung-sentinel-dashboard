bbd70ceb0fc471ade1174596d9abef4074d3f9dd
Thompson Opeyemi <Topeyemi33@gmail.com>
2025-06-03 19:13:35 +0100

    UPDATE

--- components/reususables/custom-ui/paymentReceipt.tsx
+++ components/reususables/custom-ui/paymentReceipt.tsx
@@ -212,7 +212,8 @@ export default function PaymentReceipt({ transactionData }: PaymentReceiptProps)
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                     </svg>
                   </div>
-                  <p class="text-blue-100 text-xs tracking-wide uppercase">Payment Receipt</p>
+                  <h1 class="text-blue-100 text-xs tracking-wide uppercase">Sapphire Virtual Network</h1>
+                  <p class="text-blue-100 text-xs tracking-wide uppercase">Payment Acknowledgement</p>
                   <div class="inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium ${transactionData.status === 'UNPAID' ? 'bg-red-500' : 'bg-green-500'} text-white mt-2">
                     <svg class="w-3 h-3 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
