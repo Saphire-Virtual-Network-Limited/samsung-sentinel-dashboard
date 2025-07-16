b568fbc9ea7897b5fdd340d3264292dc7ad1fcdb
Thompson Opeyemi <Topeyemi33@gmail.com>
2025-07-16 12:44:46 +0100

    updtae

---
+++ app/access/dev/devices/create/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DeviceForm } from '@/view/dashboard'
+
+const page = () => {
+  return (
+    <div>
+        <DeviceForm />
+    </div>
+  )
+}
+
+export default page
