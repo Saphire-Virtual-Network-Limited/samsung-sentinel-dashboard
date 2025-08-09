93791bec584db77686342db524f487bba8e5008e
Thompson Opeyemi <Topeyemi33@gmail.com>
2025-08-09 12:46:45 +0100

    store single page

---
+++ app/access/sales/stores/[id]/page.tsx
@@ -0,0 +1,10 @@
+import SingleStoreView from '@/view/dashboard/finance/singleStoreView'
+import React from 'react'
+
+const page = () => {
+  return (
+    <SingleStoreView />  
+  )
+}
+
+export default page
