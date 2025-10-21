ad3109119d973e6887cb7d408e7f9c3429d41e77
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2025-10-21 10:22:37 +0100

    refactor: clean up NewHomeView

--- view/dashboard/newHomeView.tsx
+++ view/dashboard/newHomeView.tsx
@@ -2,41 +2,33 @@
 
 import React from "react";
 import {
-  DashCard,
-  DailyDashCard,
-  InceptionDashCard,
-  DeviceDashAnalytic,
-  ScreenReport,
+	DashCard,
+	DailyDashCard,
+	InceptionDashCard,
+	DeviceDashAnalytic,
+	ScreenReport,
 } from "@/components/reususables";
 import { getSelectedProduct } from "@/utils";
 import MobiflexHomeView from "./MobiflexHomeView";
 import NewSalesHomeView from "../creditflex/dashboardView";
 import CommunicationLockReport from "@/components/reususables/custom-ui/CommunicationLockReport";
 const NewHomeView = () => {
-  const { label: selectedProduct } = getSelectedProduct();
+	const { label: selectedProduct } = getSelectedProduct();
 
-  if (selectedProduct == "CreditFlex") {
-    return <NewSalesHomeView />;
-  }
-    if (selectedProduct == "Mobiflex") {
-    return (  <div className="space-y-6">
-      <DailyDashCard />
-      <InceptionDashCard />
-      <DeviceDashAnalytic />
-      <ScreenReport />
-      {selectedProduct == "Mobiflex" && <MobiflexHomeView />}
-    </div>);
-  }
-  return (
-    <div className="space-y-6">
-      <DailyDashCard />
-      <CommunicationLockReport />
-      <InceptionDashCard />
-      <DeviceDashAnalytic />
-      <ScreenReport />
-      {selectedProduct == "Mobiflex" && <MobiflexHomeView />}
-    </div>
-  );
+	if (selectedProduct == "CreditFlex") {
+		return <NewSalesHomeView />;
+	}
+
+	return (
+		<div className="space-y-6">
+			<DailyDashCard />
+			<CommunicationLockReport />
+			<InceptionDashCard />
+			<DeviceDashAnalytic />
+			<ScreenReport />
+			{selectedProduct == "Mobiflex" && <MobiflexHomeView />}
+		</div>
+	);
 };
 
 export default NewHomeView;
